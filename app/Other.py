04b71ae6a135aff61f17d1056d7f6d02e8473841
ffreda22 <ffreda22@yahoo.com>
2020-10-04 14:37:28 -0400

    Initial commit for Other

---
+++ app/Other.py
@@ -0,0 +1,19 @@
+import urllib
+import re
+
+print("Hit Other")
+
+class MyClass:
+
+    def myfunction(self, x):
+        return x
+
+    @property
+    def printsomething(self):
+        return "Hey There!"
+
+    def getTesla(self):  #When no paramters passed, you still need a "self" here
+        return "TSLA"
+
+
+
